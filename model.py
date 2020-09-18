ed02059420e09b551a7847d0ffbc752da84e54c6
Hồ Nguyễn Hải Tuấn <haituan134@gmail.com>
2020-09-18 17:23:21 +0700

    define cyclegan subclass model

--- model.py
+++ model.py
@@ -107,144 +107,131 @@ def Discriminator():
 
   return tf.keras.Model(inputs=inp, outputs=last)
 
-class CycleGAN():
-  def __init__(self):
-    self.lambda_value = 10
+class CycleGan(keras.Model):
+    def __init__(
+        self,
+        monet_generator,
+        photo_generator,
+        monet_discriminator,
+        photo_discriminator,
+        lambda_cycle=10,
+    ):
+        super(CycleGan, self).__init__()
+        self.m_gen = monet_generator
+        self.p_gen = photo_generator
+        self.m_disc = monet_discriminator
+        self.p_disc = photo_discriminator
+        self.lambda_cycle = lambda_cycle
+        
+    def compile(
+        self,
+        m_gen_optimizer,
+        p_gen_optimizer,
+        m_disc_optimizer,
+        p_disc_optimizer,
+        gen_loss_fn,
+        disc_loss_fn,
+        cycle_loss_fn,
+        identity_loss_fn
+    ):
+        super(CycleGan, self).compile()
+        self.m_gen_optimizer = m_gen_optimizer
+        self.p_gen_optimizer = p_gen_optimizer
+        self.m_disc_optimizer = m_disc_optimizer
+        self.p_disc_optimizer = p_disc_optimizer
+        self.gen_loss_fn = gen_loss_fn
+        self.disc_loss_fn = disc_loss_fn
+        self.cycle_loss_fn = cycle_loss_fn
+        self.identity_loss_fn = identity_loss_fn
 
-    self.generator_g = Generator(OUTPUT_CHANNELS=3)
-    self.generator_f = Generator(OUTPUT_CHANNELS=3)
+    def call(self, inputs):
+        photo_inputs, monet_inputs = inputs
+        return self.m_gen(photo_inputs), self.p_gen(monet_inputs)
 
-    self.discriminator_x = Discriminator()
-    self.discriminator_y = Discriminator()
+    def train_step(self, batch_data):
+        real_monet, real_photo = batch_data
+        
+        with tf.GradientTape(persistent=True) as tape:
+            # photo to monet back to photo
+            fake_monet = self.m_gen(real_photo, training=True)
+            cycled_photo = self.p_gen(fake_monet, training=True)
 
-    self.generator_g_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
-    self.generator_f_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+            # monet to photo back to monet
+            fake_photo = self.p_gen(real_monet, training=True)
+            cycled_monet = self.m_gen(fake_photo, training=True)
 
-    self.discriminator_x_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
-    self.discriminator_y_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+            # generating itself
+            same_monet = self.m_gen(real_monet, training=True)
+            same_photo = self.p_gen(real_photo, training=True)
 
-    self.checkpoint = tf.train.Checkpoint(
-      generator_g = self.generator_g,
-      generator_f = self.generator_f,
-      discriminator_x = self.discriminator_x,
-      discriminator_y = self.discriminator_y,
-      generator_g_optimizer = self.generator_g_optimizer,
-      generator_f_optimizer = self.generator_f_optimizer,
-      discriminator_x_optimizer = self.discriminator_x,
-      discriminator_y_optimizer = self.discriminator_y
-    )
+            # discriminator used to check, inputing real images
+            disc_real_monet = self.m_disc(real_monet, training=True)
+            disc_real_photo = self.p_disc(real_photo, training=True)
 
-    self.loss_obj = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+            # discriminator used to check, inputing fake images
+            disc_fake_monet = self.m_disc(fake_monet, training=True)
+            disc_fake_photo = self.p_disc(fake_photo, training=True)
 
-  def discriminator_loss(self, real, generated):
-    real_loss = self.loss_obj(tf.ones_like(real), real)
-    generated_loss = self.loss_obj(tf.zeros_like(generated), generated)
-    total_disc_loss = real_loss + generated_loss
+            # evaluates generator loss
+            monet_gen_loss = self.gen_loss_fn(disc_fake_monet)
+            photo_gen_loss = self.gen_loss_fn(disc_fake_photo)
 
-    return total_disc_loss * 0.5
+            # evaluates total cycle consistency loss
+            total_cycle_loss = self.cycle_loss_fn(real_monet, cycled_monet, self.lambda_cycle) + self.cycle_loss_fn(real_photo, cycled_photo, self.lambda_cycle)
 
-  def generator_loss(self, generated):
-    return self.loss_obj(tf.ones_like(generated), generated)
+            # evaluates total generator loss
+            total_monet_gen_loss = monet_gen_loss + total_cycle_loss + self.identity_loss_fn(real_monet, same_monet, self.lambda_cycle)
+            total_photo_gen_loss = photo_gen_loss + total_cycle_loss + self.identity_loss_fn(real_photo, same_photo, self.lambda_cycle)
 
-  def calc_cycle_loss(self, real_image, cycled_image):
-    loss1 = tf.reduce_mean(tf.abs(real_image - cycled_image))
-    return self.lambda_value * loss1
+            # evaluates discriminator loss
+            monet_disc_loss = self.disc_loss_fn(disc_real_monet, disc_fake_monet)
+            photo_disc_loss = self.disc_loss_fn(disc_real_photo, disc_fake_photo)
 
-  def identity_loss(self, real_image, same_image):
-    loss = tf.reduce_mean(tf.abs(real_image - same_image))
-    return self.lambda_value * 0.5 * loss
+        # Calculate the gradients for generator and discriminator
+        monet_generator_gradients = tape.gradient(total_monet_gen_loss,
+                                                  self.m_gen.trainable_variables)
+        photo_generator_gradients = tape.gradient(total_photo_gen_loss,
+                                                  self.p_gen.trainable_variables)
 
-  @tf.function
-  def train_step(self, real_x, real_y):
-    # persistent is set to True because the tape is used more than
-    # once to calculate the gradients.
-    with tf.GradientTape(persistent=True) as tape:
-      # Generator G translates X -> Y
-      # Generator F translates Y -> X.
+        monet_discriminator_gradients = tape.gradient(monet_disc_loss,
+                                                      self.m_disc.trainable_variables)
+        photo_discriminator_gradients = tape.gradient(photo_disc_loss,
+                                                      self.p_disc.trainable_variables)
+
+        # Apply the gradients to the optimizer
+        self.m_gen_optimizer.apply_gradients(zip(monet_generator_gradients,
+                                                 self.m_gen.trainable_variables))
+
+        self.p_gen_optimizer.apply_gradients(zip(photo_generator_gradients,
+                                                 self.p_gen.trainable_variables))
+
+        self.m_disc_optimizer.apply_gradients(zip(monet_discriminator_gradients,
+                                                  self.m_disc.trainable_variables))
+
+        self.p_disc_optimizer.apply_gradients(zip(photo_discriminator_gradients,
+                                                  self.p_disc.trainable_variables))
+        
+        return {
+            "monet_gen_loss": total_monet_gen_loss,
+            "photo_gen_loss": total_photo_gen_loss,
+            "monet_disc_loss": monet_disc_loss,
+            "photo_disc_loss": photo_disc_loss
+        }
+        
+    def save(self, path):
+      if not os.path.exists(path):
+        os.makedirs(path)
+      self.m_gen.save(path + '/m_den.h5')
+      self.m_disc.save(path + '/m_dic.h5')
+      self.p_gen.save(path + '/p_gen.h5')
+      self.p_disc.save(path + '/p_dics.h5')
+
+    @staticmethod
+    def load(path):
+      result = CycleGan(None, None, None, None)
+      result.m_gen = keras.models.load_model(path + '/m_den.h5')
+      result.m_disc = keras.models.load_model(path + '/m_dic.h5')
+      result.p_gen = keras.models.load_model(path + '/p_gen.h5')
+      result.p_disc = keras.models.load_model(path + '/p_dics.h5')
       
-      fake_y = self.generator_g(real_x, training=True)
-      cycled_x = self.generator_f(fake_y, training=True)
-
-      fake_x = self.generator_f(real_y, training=True)
-      cycled_y = self.generator_g(fake_x, training=True)
-
-      # same_x and same_y are used for identity loss.
-      same_x = self.generator_f(real_x, training=True)
-      same_y = self.generator_g(real_y, training=True)
-
-      disc_real_x = self.discriminator_x(real_x, training=True)
-      disc_real_y = self.discriminator_y(real_y, training=True)
-
-      disc_fake_x = self.discriminator_x(fake_x, training=True)
-      disc_fake_y = self.discriminator_y(fake_y, training=True)
-
-      # calculate the loss
-      gen_g_loss = self.generator_loss(disc_fake_y)
-      gen_f_loss = self.generator_loss(disc_fake_x)
-      
-      total_cycle_loss = self.calc_cycle_loss(real_x, cycled_x) + self.calc_cycle_loss(real_y, cycled_y)
-      
-      # Total generator loss = adversarial loss + cycle loss
-      total_gen_g_loss = gen_g_loss + total_cycle_loss + self.identity_loss(real_y, same_y)
-      total_gen_f_loss = gen_f_loss + total_cycle_loss + self.identity_loss(real_x, same_x)
-
-      disc_x_loss = self.discriminator_loss(disc_real_x, disc_fake_x)
-      disc_y_loss = self.discriminator_loss(disc_real_y, disc_fake_y)
-    
-    # Calculate the gradients for generator and discriminator
-    generator_g_gradients = tape.gradient(total_gen_g_loss, 
-                                          self.generator_g.trainable_variables)
-    generator_f_gradients = tape.gradient(total_gen_f_loss, 
-                                          self.generator_f.trainable_variables)
-    
-    discriminator_x_gradients = tape.gradient(disc_x_loss, 
-                                              self.discriminator_x.trainable_variables)
-    discriminator_y_gradients = tape.gradient(disc_y_loss, 
-                                              self.discriminator_y.trainable_variables)
-    
-    # Apply the gradients to the optimizer
-    self.generator_g_optimizer.apply_gradients(zip(generator_g_gradients, 
-                                              self.generator_g.trainable_variables))
-
-    self.generator_f_optimizer.apply_gradients(zip(generator_f_gradients, 
-                                              self.generator_f.trainable_variables))
-    
-    self.discriminator_x_optimizer.apply_gradients(zip(discriminator_x_gradients,
-                                                  self.discriminator_x.trainable_variables))
-    
-    self.discriminator_y_optimizer.apply_gradients(zip(discriminator_y_gradients,
-                                                  self.discriminator_y.trainable_variables))
-    
-    return total_gen_g_loss, total_gen_f_loss, disc_x_loss, disc_y_loss
-  
-  def generate_images(self, test_input):
-    prediction = self.generator_g(test_input)
-    plt.figure(figsize=(12, 12))
-    display_list = [test_input[0], prediction[0]]
-    title = ['Input Image', 'Predicted Image']
-
-    for i in range(2):
-      plt.subplot(1, 2, i+1)
-      plt.title(title[i])
-      # getting the pixel values between [0, 1] to plot it.
-      plt.imshow(display_list[i] * 0.5 + 0.5)
-      plt.axis('off')
-    plt.show()
-
-  def train(self, dataset, EPOCHS, checkpoint_pr):
-    for epoch in tqdm(range(EPOCHS)):
-
-      for image_x, image_y in dataset:
-        total_gen_g_loss, total_gen_f_loss, disc_x_loss, disc_y_loss = self.train_step(image_x, image_y)
-
-      clear_output(wait=True)
-      # Using a consistent image (sample_horse) so that the progress of the model
-      # is clearly visible.
-      generate_images(generator_g, image_x[0])
-
-      if (epoch + 1) % 5 == 0:
-        save_path = self.checkpoint.save(file_prefix=checkpoint_pr)
-        print ('Saving checkpoint for epoch {} at {}'.format(epoch+1, save_path))
-
-      template = 'Epoch {}, Generator g loss {}, Generator f loss {}, Discriminator g Loss {}, Discriminator f Loss {}'
-      print (template.format(epoch, total_gen_g_loss, total_gen_f_loss, disc_x_loss, disc_y_loss))
+      return result
